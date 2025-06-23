747aaa3321991f6304d041efe2b5b667902fc6dc
KC1064 <kiron08072003@gmail.com>
2025-06-23 19:44:51 +0530

    Completed frontend

--- frontend/src/utils/api.js
+++
@@ -1,6 +0,0 @@
-import { axiosInstance } from "./axiosInstance";
-
-export const signup = async (signupData) => {
-  const res = await axiosInstance.post("/auth/signup", signupData);
-  return res.data;
-};
